20a4c1cfd948454c6a442bc77ad3e479e342cddd
lynnethP <lynnethpereira12@gmail.com>
2023-10-10 15:57:02 -0400

    Component CounterApp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 // import {HelloApp} from './HelloApp'
-import {FirstApp} from './FirstApp'
+// import {FirstApp} from './FirstApp'
+import { CounterApp } from './CounterApp';
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* <HelloApp /> */}
-        <FirstApp title='Hola, mundo!' subTitle={12345} />
+        {/* <FirstApp title='Hola, mundo!' subTitle={12345} /> */}
+        <CounterApp value={2023} />
     </React.StrictMode>
 )
